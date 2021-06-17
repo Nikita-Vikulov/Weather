f8cd70953f8cdb6f44638d7926dfe9bc05eea06b
Nikta <n.vikulov92@yandex.ru>
2021-06-17 14:55:28 +0300

    HomeWork5

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.weather.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather.R
+import com.example.weather.databinding.MainActivityBinding
 import com.example.weather.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.ok.setOnClickListener(clickListener)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+                .commitAllowingStateLoss()
         }
     }
 }
-
